6c49af119d65153ff155839daaa296037a3560f0
worichka21q31 <160734161+worichka21q31@users.noreply.github.com>
2026-04-25 12:52:09 +0500

    Update back_to_menu.gd

--- scripts/start_menu_scr/back_to_menu.gd
+++ scripts/start_menu_scr/back_to_menu.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_pressed() -> void:
+	$"../../..".data_save()
 	save_signal.emit()
 	get_tree().change_scene_to_file("res://scenes/start_menu/menu.tscn")
